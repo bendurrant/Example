e2e1951368417f7c0e10bad25760af308cc95a21
Ben Durrant <Ben Durrant>
2017-02-11 21:15:43 -0700

    Made a change in a design. I originally had planned to create an adjacency list with a dictionary. However as I started to implement realized that with one dependency per package it would be easier to store all dependencies in the node class itself. Making the implementation somewhat simpler. I then had to change my tests to incorporate this new design. I also then implemented a small piece of the code that puts the packages into a graph.

--- InstallerExercise.py
+++ InstallerExercise.py
@@ -3,18 +3,33 @@ import math
 
 
 class Node(object):
-    def __init__(self,package):
+    def __init__(self,package,dependency):
         self.package = package
         self.visited = False
         self.previous = None
+        self.depend = dependency
         self.preTime = 0
         self.postTime = 0
-
+    def __eq__(self,another):
+        return hasattr(another, "package") and self.package == another.package
+    def __hash__(self):
+        return hash(self.package)
+    
 def InstallerExercise(inputArray):
     graph = createGraph(inputArray)
+    return graph
     
 def createGraph(inputArray):
-    dag= {}
+    graph= {}
+    nodes = {}
     tempStringHolder = []
     for i in inputArray:
-        return False
+        tempStringHolder = i.split(": ")
+        print(tempStringHolder)
+        if(len(tempStringHolder[1]) == 0):
+            dependency = None
+        else:
+            dependency = tempStringHolder[1]
+        node = Node(tempStringHolder[0],dependency)
+        graph[node.package] = node
+    return graph
--- InstallerExerciseTestSuite.py
+++ InstallerExerciseTestSuite.py
@@ -1,34 +1,36 @@
 import unittest
 from InstallerExercise import InstallerExercise
+from InstallerExercise import Node
 
 class InstallerExerciseTestMethods(unittest.TestCase):
-
+    maxDiff = None
     def testExpectedStringParse(self):
-        testArray = ["French Fries: Potatos","Bacon: Pigs"]
+        testArray = ["French Fries: Potatos","Bacon: "]
         testArrayASGraph= {};
-        testArrayASGraph["French Fries"] = "Potato"
-        testArrayASGraph["Bacon"] = "Pigs"
-        self.assertEqual(testArrayASGraph, InstallerExercise(testArray))
+        testArrayASGraph["French Fries"] = Node("French Fries", "Potatos")
+        testArrayASGraph["Bacon"] = Node("Bacon",None)
+        self.assertDictEqual(testArrayASGraph, InstallerExercise(testArray))
         
     def testSingleElementInGraph(self):
         testArray = ["Bacon: Pigs"]
         testArrayASGraph= {};
-        testArrayASGraph["Bacon"] = "Pigs"
-        self.assertEqual(testArrayASGraph, InstallerExercise(testArray))
+        testArrayASGraph["Bacon"] = Node("Bacon","Pigs")
+        self.assertDictEqual(testArrayASGraph, InstallerExercise(testArray))
+       
     def testLargeNumOfElementInGraph(self):
-        testArray = ["French Fries: Potatos","Bacon: Pigs","KittenService: CamelCaser",
+        testArray = ["French Fries: Potatos","Bacon: Pigs","KittenService: ",
                      "Leetmeme: Cyberportal","Cyberportal: Ice","CamelCaser: KittenService",
                      "Fraudstream: Leetmeme","Ice: "]
         testArrayASGraph= {}
-        testArrayASGraph["French Fries"] = "Potato"
-        testArrayASGraph["Bacon"] = "Pigs"
-        testArrayASGraph["KittenService"] = None
-        testArrayASGraph["Leetmeme"] = "Cyberportal"
-        testArrayASGraph["Cyberportal"] = "Ice"
-        testArrayASGraph["CamelCaser"] = "KittenService"
-        testArrayASGraph["Fraudstream"] = "Leetmeme"
-        testArrayASGraph["Ice"] = None
-        self.assertEqual(testArrayASGraph, InstallerExercise(testArray))
+        testArrayASGraph["French Fries"] = Node("French Fries", "Potatos")
+        testArrayASGraph["Bacon"] = Node("Bacon","Pigs")
+        testArrayASGraph["KittenService"] = Node("KittenService",None)
+        testArrayASGraph["Leetmeme"] = Node("Leetmeme","Cyberportal")
+        testArrayASGraph["Cyberportal"] = Node("Cyberportal","Ice")
+        testArrayASGraph["CamelCaser"] = Node("CamelCaser","KittenService")
+        testArrayASGraph["Fraudstream"] = Node("Fraudstream","Leetmeme")
+        testArrayASGraph["Ice"] = Node("Ice",None)
+        self.assertDictEqual(testArrayASGraph, InstallerExercise(testArray))
 
 if __name__=='__main__':
     unittest.main()
